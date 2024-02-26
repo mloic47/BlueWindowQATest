21b7b669b0858b0a12c4d676ca21e1452e326177
leroyloic <loic.mewoulou@cm.maviance.com>
2024-02-26 23:40:30 +0100

    Add submit button element to CreateUserProfilePage

--- pages/CreateUserProfilePage.js
+++ pages/CreateUserProfilePage.js
@@ -1,8 +1,11 @@
 
 export class CreateUserProfilePage {
-    async createProfile(page) {
-        await page.fill('input[name="name"]', 'John Doe');
-        await page.fill('input[name="email"]', 'john@gmail.com');
+    
+    // Page Elements
+    constructor() {
+        this.submitButton = 'input[type="submit"]';
+
+        
     }
   
 }
